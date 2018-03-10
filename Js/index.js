f78c869546e243273f678149b614aa0dbee5b0c2
AMIT MISHRA <500052926@stu.upes.ac.in>
2018-03-10 16:11:56 +0530

    Update index.js
    added snake and ladder condition.

--- Js/index.js
+++ Js/index.js
@@ -8,22 +8,120 @@ $(document).ready(function(){
     function dice_roll(dice_no, plyno){
         var player = "player"+plyno;
         posp=pos+eval(player);
-        alert(posp);
         $(posp).removeClass(player);
-        alert(posp);
         posn = pos+(eval(player)+dice_no);
         
         if(plyno == 1)
-            player1+=dice_no;
+           { player1+=dice_no;
+            if(player1 > 36)
+                player1-=dice_no;
+            else
+                {
+                    if(player1 == 36)
+                    {
+                        win(1);
+                    }
+                    else
+                    {
+                        snake(player1, 1);
+                        ladder(player1, 1);
+                        posn = pos+player1;
+                    }
+                }
+            
+            
+           }
         else
+        {
             player2+=dice_no;
-        
-        posp=posn;
-        alert(posn);
+            if(player2>36)
+                player2-=dice_no;
+            else
+                {
+                     if(player2 == 36)
+                     {
+                         win(2);
+                     }
+                    else
+                    {
+                        snake(player2, 2);
+                        ladder(player2, 2);
+                        posn = pos +player2;
+                    }
+                    
+                }
+           
+        }
         $(posn).addClass(player);
     }
     
-    function dice_move(){
+    function snake(position, plyno){
+        switch(position)
+            {
+                case 11: if(plyno == 1)
+                    player1=3;
+                    else
+                        player2=3;
+                    
+                    break;
+                
+                case 23: if(plyno ==1)
+                    player1=16;
+                    else
+                        player2=16;
+                    
+                    break;
+                    
+                case 25:
+                    if(plyno ==1 )
+                        player1=20;
+                    else
+                        player2=20;
+                    
+                    break;
+                    
+                case 35: 
+                    if(plyno == 1)
+                        player1=28;
+                    else
+                        player2=28;
+                    
+                    break;
+            }
+        
+    }
+    function ladder(position, plyno){
+        switch(position)
+            {
+                case 6: 
+                    if(plyno == 1)
+                        player1=18;
+                    else
+                        player2=18;
+                    
+                    break;
+                
+                case 12:
+                    if(plyno == 1)
+                        player1=19;
+                    else
+                        player2=19;
+                    
+                    break;
+                
+                case 15: 
+                    if(plyno == 1)
+                        player1=27;
+                    else
+                        player2=27;
+                    
+                    break;
+            }
+        
+    }
+    function win(plyno)
+    {
+        alert("Player"+plyno+"is the winner!");
         
     }
     $(".selector").click(function(){
@@ -38,7 +136,7 @@ $(document).ready(function(){
                     var dice = Math.floor(Math.random()*6)+1;
                     var res = "#dice"+dice;
                     $(res).show(6000);
-                    
+                    $(res).hide(7000);
                     dice_roll(dice, 1);
                    var dice = Math.floor(Math.random()*6)+1;
                     dice_roll(dice, 2);
